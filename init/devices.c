Xmister/platform_system_core

System-core-init: Implement device triggers
Android system/core/init/readme describes two triggers, which are
not implemented in the code yet.
They are:
device-added-<path>
device-removed-<path>
Triggers of these forms occur when a device node is added or removed.
These triggers are very important for PnP devices. For example when
system goes to suspend, the inbuilt usb in some devices is power-off
causing whole driver to be removed. When they are driver restarts on
resume the sysfs files permission will be reset to root again. This
will cause processes with non-root access to fail.
These triggers allows to change ownership again.
Android ueventd.*.rc file allows to modify permissions of file
matching the path sent by udev event, but doesn't allow to set
permissions in sysfs files or other files.
Also this patch goes through device triggers at the end of boot
process and fires them to account for uevents before the init
process was created.

Change-Id: Ib7f47124f324fdba4da332c7d1ea5040f45eeb13
Signed-off-by: srinivas pandruvada <srinivas.pandruvada@intel.com>
Signed-off-by: Daniel Leung <daniel.leung@intel.com>

init/devices.c
@@ -21,7 +21,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
+#include <sys/system_properties.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <unistd.h>
@@ -499,6 +499,7 @@ static void handle_device(const char *action, const char *devpath,
 
     if(!strcmp(action, "add")) {
         make_device(devpath, path, block, major, minor);
+        __system_property_set("ctl.dev_added",devpath);
         if (links) {
             for (i = 0; links[i]; i++)
                 make_link(devpath, links[i]);
@@ -510,6 +511,7 @@ static void handle_device(const char *action, const char *devpath,
             for (i = 0; links[i]; i++)
                 remove_link(devpath, links[i]);
         }
+        __system_property_set("ctl.dev_removed",devpath);
         unlink(devpath);
     }
 

iConsole/Console-OS_system_core

init/devices.c
@@ -21,7 +21,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
+#include <sys/system_properties.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <unistd.h>
@@ -562,6 +562,7 @@ static void handle_device(const char *action, const char *devpath,
 
     if(!strcmp(action, "add")) {
         make_device(devpath, path, block, major, minor, (const char **)links);
+        __system_property_set("ctl.dev_added", devpath);
         if (links) {
             for (i = 0; links[i]; i++)
                 make_link_init(devpath, links[i]);
@@ -573,6 +574,7 @@ static void handle_device(const char *action, const char *devpath,
             for (i = 0; links[i]; i++)
                 remove_link(devpath, links[i]);
         }
+        __system_property_set("ctl.dev_removed", devpath);
         unlink(devpath);
     }
 